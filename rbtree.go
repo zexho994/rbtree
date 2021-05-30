eea3a0b6df3782d7c1517b42b05e52132cf04765
zouzhihao-994 <zouzhihao994@gmail.com>
2021-05-30 20:36:21 +0800

    添加左旋操作

--- rbtree.go
+++ rbtree.go
@@ -33,5 +33,69 @@ func (t *rbTree) Find(v V) *node {
 }
 
 func (t *rbTree) Insert(v V) {
+	node := t.Find(v)
+	if node.IsNonLeaf() {
+		return
+	}
 
+	// 保存新的节点到对应的位置
+	parent := node.Parent()
+	if v < parent.Val() {
+		parent.SetLeft(NewRedNode(v))
+	} else {
+		parent.SetRight(NewRedNode(v))
+	}
+
+CASE1:
+	if IsRoot(node) || node.Parent().IsBlack() {
+		return
+	}
+
+	if node.Uncle() != nil && node.Uncle().IsRed() {
+		node.Parent().TurnBlack()
+		node.Uncle().TurnBlack()
+		if IsRoot(node.Grandfather()) {
+			return
+		}
+		node.Grandfather().TurnRed()
+		node = node.Grandfather()
+		goto CASE1
+	}
+
+	if !((node.IsLeft() && node.Parent().IsLeft()) || (node.IsRight() && node.Parent().IsRight())) {
+		if node.IsLeft() {
+
+		} else {
+
+		}
+	}
+
+	node.Parent().TurnRed()
+	node.Grandfather().TurnBlack()
+	if node.IsLeft() {
+
+	} else {
+
+	}
+
+}
+
+func IsRoot(n *node) bool {
+	return n.parent == nil
+}
+
+func leftRotate(x *node) bool {
+	var c *node
+	if c = x.Right(); c.IsLeaf() {
+		return false
+	}
+	if x.IsLeft() {
+		x.Parent().SetLeft(c)
+	} else {
+		x.Parent().SetRight(c)
+	}
+
+	x.SetRight(c.Left())
+	c.SetLeft(x)
+	return true
 }
--- rbtree_test.go
+++ rbtree_test.go
@@ -17,10 +17,11 @@ func TestRbTree_Find(t *testing.T) {
 	AssertIsTrue(t1.Find(4).IsLeaf())
 }
 
-// 				11                  11
-//            4    20             4    20
-//          2   6         ->    2  6
-//        1                    1 3
+// 				 11b                   11b             11b
+//            4r    20r             4r    20r        4b   20b
+//          2b   6b         ->    2b  6b       ->   2r 6b
+//        1r                    1r 3r             1b 3b
+//                                               0r
 func TestRbTree_Insert(t *testing.T) {
 	r := NewRbTree(11)
 	r.Root().SetLeft(NewRedNode(4))
@@ -38,4 +39,39 @@ func TestRbTree_Insert(t *testing.T) {
 	AssertIsTrue(f.Uncle().Val() == 6)
 	AssertIsTrue(r.Find(4).Uncle() == nil)
 	AssertIsTrue(r.Find(11).Uncle() == nil)
+
+	r.Insert(0)
+	AssertIsTrue(r.Find(1).IsBlack())
+	AssertIsTrue(r.Find(3).IsBlack())
+	AssertIsTrue(r.Find(2).IsRed())
+	AssertIsTrue(r.Find(6).IsBlack())
+	AssertIsTrue(r.Find(4).IsBlack())
+	AssertIsTrue(r.Find(20).IsBlack())
+	AssertIsTrue(r.Find(11).IsBlack())
+}
+
+//  	   	 11b                  11b
+//        4b     20b            6b   20b
+//      2r   6b        ->     4b 7r
+//    1b 3b 5r 7r		    2r 5r
+func Test_LeftRotate(t *testing.T) {
+	r := NewRbTree(11)
+	r.Root().SetLeft(NewRedNode(4))
+	r.Root().SetRight(NewRedNode(20))
+	r.Root().Left().SetLeft(NewBlackNode(2))
+	r.Root().Left().SetRight(NewBlackNode(6))
+	r.Root().Left().Left().SetLeft(NewRedNode(1))
+	r.Root().Left().Right().SetLeft(NewRedNode(5))
+	r.Root().Left().Right().SetRight(NewRedNode(7))
+	r.Insert(3)
+
+	AssertIsTrue(leftRotate(r.Find(4)))
+	n6 := r.Find(6)
+	AssertIsTrue(IsRoot(n6.Parent()))
+	AssertIsTrue(n6.Left().Val() == 4)
+	AssertIsTrue(n6.Right().Val() == 7)
+	AssertIsTrue(n6.Left().Left().Val() == 2)
+	AssertIsTrue(n6.Left().Right().Val() == 5)
+
+	AssertIsTrue(!leftRotate(r.Find(20)))
 }
